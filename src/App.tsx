47f528392458d6e72133e56fcc531e8e4c38b49a
lmaxxx <leshchenko.maksym10@gmail.com>
2022-02-26 20:27:19 +0200

    Add UnitOfTemperature Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import {Routes, Route} from 'react-router-dom'
 import Main from "./pages/Main/Main";
 import Weather from "./pages/Weather/Weather";
+import {createContext, useState} from "react";
+import {IUnitOfTemperatureContext, UnitOfTemperature} from "./types"
+
+export const UnitOfTemperatureContext = createContext<IUnitOfTemperatureContext>({} as IUnitOfTemperatureContext)
 
 function App() {
+  const [unitOfTemperature, setUnitOfTemperature] = useState<UnitOfTemperature>("c")
+
+  const unitOfTemperatureValue: IUnitOfTemperatureContext = {
+    unitOfTemperature, setUnitOfTemperature
+  }
+
   return (
-    <Routes>
-      <Route path={"/"} element={<Main/>}/>
-      <Route path={"/:city"} element={<Weather/>}/>
-    </Routes>
+    <UnitOfTemperatureContext.Provider value={unitOfTemperatureValue}>
+      <Routes>
+        <Route path={"/"} element={<Main/>}/>
+        <Route path={"/:city"} element={<Weather/>}/>
+      </Routes>
+    </UnitOfTemperatureContext.Provider>
   )
 }
 
