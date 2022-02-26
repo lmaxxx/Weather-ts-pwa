8811de855d74169c1d4e9d8372e9b33d0d82aa0d
lmaxxx <leshchenko.maksym10@gmail.com>
2022-02-26 20:30:44 +0200

    Create useForecast hook

---
+++ src/hooks/useForecast.tsx
@@ -0,0 +1,41 @@
+import {useCallback, useState} from "react";
+import {IForecast} from "../types";
+import axios from "axios";
+
+const useForecast = () => {
+  const [forecast, setForecast] = useState<IForecast>({} as IForecast)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
+
+  const getForecast = useCallback(
+    async (cityName: string | undefined) => {
+      if(!cityName) return
+
+      try {
+        setIsLoading(true)
+        setError("")
+
+        const response = await axios({
+          method: 'GET',
+          url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+          params: {q: cityName, days: '3'},
+          headers: {
+            'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
+            'x-rapidapi-key': '906b487883msh3185b1e59ac2549p1442aajsne0e636f4abac'
+          }
+        })
+
+        setForecast(response.data)
+      } catch (err: any) {
+        setError(err.message)
+      }
+      finally {
+        setIsLoading(false)
+      }
+    }, []
+  )
+
+  return {error, isLoading, forecast, getForecast}
+}
+
+export default useForecast
