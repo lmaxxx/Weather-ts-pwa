33416c83d20adbaa423bbb8c02606a87c2274db4
lmaxxx <leshchenko.maksym10@gmail.com>
2022-02-28 10:30:44 +0200

    Improve useUnitOfTemperature hook

--- src/hooks/useUnitOfTemperature.tsx
+++ src/hooks/useUnitOfTemperature.tsx
@@ -2,7 +2,24 @@ import {UnitOfTemperatureContext} from "../App"
 import {useContext} from "react";
 
 const useUnitOfTemperature = () => {
-  return useContext(UnitOfTemperatureContext);
+  const {unitOfTemperature, setUnitOfTemperature} = useContext(UnitOfTemperatureContext);
+
+  const getUnitLetter = () => {
+    if(unitOfTemperature === 'f') return "℉"
+
+    return "℃"
+  }
+
+  const toggleUnit = () => {
+    if(unitOfTemperature === 'f') {
+      setUnitOfTemperature("c")
+      return
+    }
+
+    setUnitOfTemperature("f")
+  }
+
+  return {getUnitLetter, unitOfTemperature, toggleUnit}
 }
 
 export default useUnitOfTemperature
