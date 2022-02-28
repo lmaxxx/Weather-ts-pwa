39428b4bfaf328fbccc8907cb71182641edea6ac
lmaxxx <leshchenko.maksym10@gmail.com>
2022-02-28 13:10:32 +0200

    Add first UI component (Region)

---
+++ src/UI/Region.tsx
@@ -0,0 +1,19 @@
+import {FC} from 'react'
+import {Text} from "@chakra-ui/react";
+
+interface PropsType {
+  region: string
+  country: string
+  [x: string]: any;
+}
+
+const Region: FC<PropsType> = ({region, country, ...rest}) => {
+  return (
+    <Text {...rest}>
+      {region.trim() && `${region} / `}
+      {country}
+    </Text>
+  )
+}
+
+export default Region
