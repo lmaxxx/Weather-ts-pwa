d322e1a8167ba83e95d98806aa68944ddb9e87bd
lmaxxx <leshchenko.maksym10@gmail.com>
2022-02-28 14:44:49 +0200

    Add helper function getCurrentForecastDetails

---
+++ src/helpers/getCurrentForecastDetails.tsx
@@ -0,0 +1,48 @@
+import {ICurrentForecast} from "../types";
+
+const getCurrentForecastDetails = (
+  currentForecast: ICurrentForecast,
+) => [
+  {
+    name: 'Wind speed',
+    value: currentForecast?.wind_kph,
+    unit: "kph",
+  },
+  {
+    name: 'Wind direction',
+    value: currentForecast?.wind_dir,
+    unit: '',
+  },
+  {
+    name: 'Air pressure',
+    value: currentForecast?.pressure_mb,
+    unit: 'mb',
+  },
+  {
+    name: 'Precipitation',
+    value: currentForecast?.precip_mm,
+    unit: 'mm',
+  },
+  {
+    name: 'Humidity',
+    value: currentForecast?.humidity,
+    unit: '%',
+  },
+  {
+    name: 'Cloud cover',
+    value: currentForecast?.cloud,
+    unit: '%',
+  },
+  {
+    name: 'Cloud cover',
+    value: currentForecast?.cloud,
+    unit: '%',
+  },
+  {
+    name: 'Visibility',
+    value: currentForecast?.vis_km,
+    unit: 'km',
+  },
+]
+
+export default getCurrentForecastDetails
