da6bf6d5092a55b141d3c5b8ffbbfd0a34cf9373
lmaxxx <leshchenko.maksym10@gmail.com>
2022-02-28 15:34:47 +0200

    Add HOC for routes animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useLocation} from 'react-router-dom'
 import Main from "./pages/Main/Main";
 import Weather from "./pages/Weather/Weather";
 import {createContext, useState} from "react";
 import {IUnitOfTemperatureContext, UnitOfTemperature} from "./types"
+import {AnimatePresence} from "framer-motion";
 
 export const UnitOfTemperatureContext = createContext<IUnitOfTemperatureContext>({} as IUnitOfTemperatureContext)
 
 function App() {
   const [unitOfTemperature, setUnitOfTemperature] = useState<UnitOfTemperature>("c")
+  const location = useLocation()
 
   const unitOfTemperatureValue: IUnitOfTemperatureContext = {
     unitOfTemperature, setUnitOfTemperature
@@ -15,10 +17,12 @@ function App() {
 
   return (
     <UnitOfTemperatureContext.Provider value={unitOfTemperatureValue}>
-      <Routes>
-        <Route path={"/"} element={<Main/>}/>
-        <Route path={"/:city"} element={<Weather/>}/>
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.key}>
+          <Route path={"/"} element={<Main/>}/>
+          <Route path={"/:city"} element={<Weather/>}/>
+        </Routes>
+      </AnimatePresence>
     </UnitOfTemperatureContext.Provider>
   )
 }
